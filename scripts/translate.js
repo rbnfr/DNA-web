076b5b9b4477e968942d6133172e523d596ac26e
CORP\rfrancoa <ruben.f.alcaraz@gmail.com>
2017-09-20 13:58:41 +0200

    No message

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,19 +1,32 @@
 $('.submit-button').click(function() {
-  var sequence = $('form.sequence').text();
-  var type = $('form.radio').val();
-  if (type == "RNA") {
-    var trans = translateRNA(sequence);
-    $('form.result').text(trans);
-  }
-  else if (type == "Protein") {
-    var trans = translatePROT(sequence);
-    $('form.result').text(trans);
-  }
+  var sequence = getSequence();
+  //alert(sequence);
+
+  var type = getType();
+  alert(type);
+  // if (type == "RNA") {
+  //   var trans = translateRNA(sequence);
+  //   $('form.result').text(trans);
+  // }
+  // else if (type == "Protein") {
+  //   var trans = translatePROT(sequence);
+  //   $('form.result').text(trans);
+  // }
 });
+function getSequence() {
+  var form = document.forms["conversor"];
+  var seq = form[1].value;
+  return seq;
+}
 
-function translateRNA(sequence){
-  
-};
-function translatePROT(sequence){
-
-};
+function getType(){
+  var form = document.forms["conversor"];
+  var type = form[3].attr('name');
+}
+// function translateRNA(sequence){
+//
+//
+// };
+// function translatePROT(sequence){
+//
+// };
