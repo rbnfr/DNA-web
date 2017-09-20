011e616c8e1358802e716923e6bd8f5c5fa5d145
CORP\rfrancoa <ruben.f.alcaraz@gmail.com>
2017-09-20 16:09:07 +0200

    Funcion translateRNA

--- scripts/translate.js
+++ scripts/translate.js
@@ -2,31 +2,41 @@ $('.submit-button').click(function() {
   var sequence = getSequence();
   //alert(sequence);
 
-  var type = getType();
-  alert(type);
-  // if (type == "RNA") {
-  //   var trans = translateRNA(sequence);
-  //   $('form.result').text(trans);
-  // }
-  // else if (type == "Protein") {
-  //   var trans = translatePROT(sequence);
-  //   $('form.result').text(trans);
-  // }
+  var type = $('input[name="type"]:checked').val();
+
+  if (type == "RNA") {
+    var trans = translateRNA(sequence);
+    // alert("RNA");
+    $('.result').text(trans);
+  }
+  else if (type == "Protein") {
+    // var trans = translatePROT(sequence);
+    // alert("Protein");
+    $('.result').text(type);
+  }
 });
+
+
 function getSequence() {
   var form = document.forms["conversor"];
   var seq = form[1].value;
   return seq;
 }
 
-function getType(){
-  var form = document.forms["conversor"];
-  var type = form[3].attr('name');
-}
-// function translateRNA(sequence){
-//
-//
-// };
+
+function translateRNA(sequence){
+  var seq = sequence;
+  var length = seq.length;
+  alert(length);
+  var trans = "";
+  var i;
+  for (var i = 0; i < length; i++) {
+    var codon = seq.substring(0,i+3);
+    trans += codon;
+  }
+  return trans;
+
+};
 // function translatePROT(sequence){
 //
 // };
