1de4aafc48ec7f29b9abd97d8b56907484ded864
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-09-27 10:40:50 +0200

    Added properties functions

--- scripts/translate.js
+++ scripts/translate.js
@@ -19,20 +19,14 @@ $('.submit-button').click(function() {
     var duration = after-before;
     var letters = protein[0];
     var chain = protein[1];
+    var three_letter = protein[2];
     $('.result1').val(letters);
     $('.result2').val(chain);
+    $('.result3').val(three_letter);
     $('#time').text(duration+' miliseconds');
     }
 });
 
-$('#input').keypress(function(event) {
-  var code = (event.KeyCode ? event.KeyCode : event.which);
-
-  if (code == 13) {
-    alert(code + " --> Enter");
-  }
-
-});
 
 // $('#save-button').click(function() {
 //   var translation_text = "RNA sequence: " + $('#seq-id').val();
@@ -113,7 +107,8 @@ String.prototype.replaceAt=function(index, replacement) {
 
 function translatePROT(sequence, mutate, frequency){
   var protein = "";
-  var chain = ""
+  var chain = "";
+  var three_letter = "";
   var i;
   var sequence = sequence.toUpperCase().replace(/\s/g, '');
   $('#seq-id').val(sequence);
@@ -132,29 +127,29 @@ function translatePROT(sequence, mutate, frequency){
   for (var i = 0; i < (sequence.length-2); i=i+3) {
     var codon = sequence.substring(i, i+3);
 
-    if      ( /GC./i.test(codon))        { protein += 'A'; chain += 'Alanine'+'-'; }         //Alanine
-    else if ( /TG[TC]/i.test(codon))     { protein += 'C'; chain += 'Cysteine'+'-';}         // Cysteine
-    else if ( /GA[TC]/i.test(codon))     { protein += 'D'; chain += 'Aspartic_Acid'+'-';}    // Aspartic Acid
-    else if ( /GA[AG]/i.test(codon))     { protein += 'E'; chain += 'Glutamic_Acid'+'-';}    // Glutamic Acid
-    else if ( /TT[TC]/i.test(codon))     { protein += 'F'; chain += 'Phenylalanine'+'-';}    // Phenylalanine
-    else if ( /GG./i.test(codon))        { protein += 'G'; chain += 'Glycine'+'-';}          // Glycine
-    else if ( /CA[TC]/i.test(codon))     { protein += 'H'; chain += 'Histidine'+'-';}        // Histidine
-    else if ( /AT[TCA]/i.test(codon))    { protein += 'I'; chain += 'Isoleucine'+'-';}       // Isoleucine
-    else if ( /AA[AG]/i.test(codon))     { protein += 'K'; chain += 'Lysine'+'-';}           // Lysine
-    else if ( /TT[AG]|CT./i.test(codon)) { protein += 'L'; chain += 'Leucine'+'-';}          // Leucine
-    else if ( /ATG/i.test(codon))        { protein += 'M'; chain += 'Methionine'+'-';}       // Methionine
-    else if ( /AA[TC]/i.test(codon))     { protein += 'N'; chain += 'Asparagine'+'-';}       // Asparagine
-    else if ( /CC./i.test(codon))        { protein += 'P'; chain += 'Proline'+'-';}          // Proline
-    else if ( /CA[AG]/i.test(codon))     { protein += 'Q'; chain += 'Glutamine'+'-';}        // Glutamine
-    else if ( /CG.|AG[AG]/i.test(codon)) { protein += 'R'; chain += 'Arginine'+'-';}         // Arginine
-    else if ( /TC.|AG[TC]/i.test(codon)) { protein += 'S'; chain += 'Serine'+'-';}           // Serine
-    else if ( /AC./i.test(codon))        { protein += 'T'; chain += 'Threonine'+'-';}        // Threonine
-    else if ( /GT./i.test(codon))        { protein += 'V'; chain += 'Valine'+'-';}           // Valine
-    else if ( /TGG/i.test(codon))        { protein += 'W'; chain += 'Tryptophan'+'-';}       // Tryptophan
-    else if ( /TA[TC]/i.test(codon))     { protein += 'Y'; chain += 'Tyrosine'+'-';}         // Tyrosine
-    else if ( /TA[AG]|TGA/i.test(codon)) { protein += '_'; chain += 'STOP'+'-';}             // Stop
+    if      ( /GC./i.test(codon))        { protein += 'A'; chain += 'Alanine'+'-';       three_letter += "Ala-"} //Alanine
+    else if ( /TG[TC]/i.test(codon))     { protein += 'C'; chain += 'Cysteine'+'-';      three_letter += "Cys-"} // Cysteine
+    else if ( /GA[TC]/i.test(codon))     { protein += 'D'; chain += 'Aspartic_Acid'+'-'; three_letter += "Asx-"} // Aspartic Acid
+    else if ( /GA[AG]/i.test(codon))     { protein += 'E'; chain += 'Glutamic_Acid'+'-'; three_letter += "Glx-"} // Glutamic Acid
+    else if ( /TT[TC]/i.test(codon))     { protein += 'F'; chain += 'Phenylalanine'+'-'; three_letter += "Phe-"} // Phenylalanine
+    else if ( /GG./i.test(codon))        { protein += 'G'; chain += 'Glycine'+'-';       three_letter += "Gly-"} // Glycine
+    else if ( /CA[TC]/i.test(codon))     { protein += 'H'; chain += 'Histidine'+'-';     three_letter += "His-"} // Histidine
+    else if ( /AT[TCA]/i.test(codon))    { protein += 'I'; chain += 'Isoleucine'+'-';    three_letter += "Ile-"} // Isoleucine
+    else if ( /AA[AG]/i.test(codon))     { protein += 'K'; chain += 'Lysine'+'-';        three_letter += "Lys-"} // Lysine
+    else if ( /TT[AG]|CT./i.test(codon)) { protein += 'L'; chain += 'Leucine'+'-';       three_letter += "Leu-"} // Leucine
+    else if ( /ATG/i.test(codon))        { protein += 'M'; chain += 'Methionine'+'-';    three_letter += "Met-"} // Methionine
+    else if ( /AA[TC]/i.test(codon))     { protein += 'N'; chain += 'Asparagine'+'-';    three_letter += "Asn-"} // Asparagine
+    else if ( /CC./i.test(codon))        { protein += 'P'; chain += 'Proline'+'-';       three_letter += "Pro-"} // Proline
+    else if ( /CA[AG]/i.test(codon))     { protein += 'Q'; chain += 'Glutamine'+'-';     three_letter += "Gln-"} // Glutamine
+    else if ( /CG.|AG[AG]/i.test(codon)) { protein += 'R'; chain += 'Arginine'+'-';      three_letter += "Arg-"} // Arginine
+    else if ( /TC.|AG[TC]/i.test(codon)) { protein += 'S'; chain += 'Serine'+'-';        three_letter += "Ser-"} // Serine
+    else if ( /AC./i.test(codon))        { protein += 'T'; chain += 'Threonine'+'-';     three_letter += "Thr-"} // Threonine
+    else if ( /GT./i.test(codon))        { protein += 'V'; chain += 'Valine'+'-';        three_letter += "Val-"} // Valine
+    else if ( /TGG/i.test(codon))        { protein += 'W'; chain += 'Tryptophan'+'-';    three_letter += "Trp-"} // Tryptophan
+    else if ( /TA[TC]/i.test(codon))     { protein += 'Y'; chain += 'Tyrosine'+'-';      three_letter += "Tyr-"} // Tyrosine
+    else if ( /TA[AG]|TGA/i.test(codon)) { protein += '_'; chain += 'STOP'+'-';          three_letter += "STOP-"}// Stop
     else {alert("Unrecognized codon starting at position " + i + " : " + codon);}
   }
 
-  return [protein, chain];
+  return [protein, chain, three_letter];
 }
