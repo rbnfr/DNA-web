73790115f62e1b1f95bb790c99f4a5d0c312b25b
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-10-04 12:25:42 +0200

    Fixed switch
    Now, change switch state, enables or disables the frequency field.

--- scripts/generate_seq.js
+++ scripts/generate_seq.js
@@ -1,31 +1,30 @@
-$('#cleaner').click(function() {
-  $('#seq-id').val("");
-  $('#seq-id').trigger('autoresize');
+$('#cleaner').click(function () {
+    $('#seq-id').val("");
+    $('#seq-id').trigger('autoresize');
 });
 
 
-$('#generator').click(function(){
-  var seq_length = $('#seq_length').val();
+$('#generator').click(function () {
+    var seq_length = $('#seq_length').val();
 
-  if (seq_length) {
-    var sequence = createSequence(seq_length);
-  }
-  else {
-    var sequence = createSequence(Math.floor((Math.random() * 1000) + 3));
-  }
-  $('#seq-id').val(sequence);
-  $('#seq-id').trigger('autoresize');
+    if (seq_length) {
+        var sequence = createSequence(seq_length);
+    } else {
+        var sequence = createSequence(Math.floor((Math.random() * 1000) + 3));
+    }
+    $('#seq-id').val(sequence);
+    $('#seq-id').trigger('autoresize');
 });
 
-function createSequence(size){
-  var sequence = "";
-  for(var i = 0 ; i < size; i++){
-    sequence += getRandomChar();
-  }
-  return sequence;
+function createSequence(size) {
+    var sequence = "";
+    for (var i = 0; i < size; i++) {
+        sequence += getRandomChar();
+    }
+    return sequence;
 }
 
-function getRandomChar(){
-  var chars = ["A","T","C","G"];
-  return chars[Math.floor((Math.random() * chars.length))];
+function getRandomChar() {
+    var chars = ["A", "T", "C", "G"];
+    return chars[Math.floor((Math.random() * chars.length))];
 }
